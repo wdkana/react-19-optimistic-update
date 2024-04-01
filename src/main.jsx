297a07b4215479807aab93440a0034d196af2404
deaaprizal <afrizaldea94@gmail.com>
2024-04-01 16:32:04 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import OptimisticApp from './App2.jsx'
+import OptimisticApp from './OptimisticApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
